2fa62b361ebf1363518967ef33463e8c8f105262
Ben Schwartz <btschwartz12@gmail.com>
2023-06-19 08:42:46 -0400

    Added redirect countdown

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 
 
-
+import { AwesomeButton } from "react-awesome-button";
 import { MovingElement } from '../components/MovingElement.jsx';
 import Buttons from '../components/Buttons.jsx';
 import '../styles/App.css';
@@ -15,7 +15,7 @@ import { Explanation } from '../misc/Toasts.jsx'
 import Loader from '../misc/Preloader.jsx';
 import { useContext } from 'react';
 import AnimationContext from '../misc/AnimationContext.jsx';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ContactModal from '../misc/ContactModal.jsx';
 
 
@@ -40,7 +40,48 @@ function getRandomType() {
 
 
 
+const buttonData = [
+  {
+    text: 'What?',
+    row: 1,
+    toast: Explanation,
+    className: 'gold'
 
+  },
+  {
+    text: 'Portfolio',
+    link: 'https://btschwartz.com/portfolio/',
+    row: 2,
+    className: 'purple'
+
+  },
+  {
+    text: 'Resume',
+    link: 'https://drive.google.com/file/d/1wCPzd7fiAko-PfaizeCkd8ZChVdLK7eA/view?usp=sharing',
+    row: 2,
+    className: 'black'
+  },
+  
+
+  {
+    text: 'More',
+    row: 3,
+    className: 'gray',
+    navLink: '/more',
+    link: '/more'
+
+  },
+  
+  {
+    text: 'Contact',
+    row: 3,
+    className: 'red',
+    modal: ContactModal,
+    modalId: 'contact',
+    modalProps: {
+    },
+  }
+]
 
 
 
@@ -84,73 +125,90 @@ const Tagline = () => {
   );
 };
 
+const Countdown = ({ onEnd, onCancel }) => {
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    if (countdown < 1) {
+      return;
+    }
+    
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000); // Update every 1 second
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [countdown]);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      onEnd();
+    }
+  }, [countdown, onEnd]);
+
+  return (
+    <>
+    <div className="my-container">
+      <MovingElement key={countdown} type='zoomIn' element={() =>
+        <div className='intro'>
+          {countdown > 0 ? countdown : 0}
+        </div>
+      }/>
+    </div>
+    <div className='my-container'>
+      <MovingElement type='zoomIn' element={() =>
+          <div className="tagline">
+            Redirecting to my portfolio...
+          </div>}>
+        </MovingElement>
+    </div>
+    <div className='my-container'>
+      <MovingElement type='slideInFromTop' element={() =>
+
+        <AwesomeButton
+            type='primary'
+            className={`aws-btn purple`}
+            onPress={onCancel}
+        >
+          Stop!
+        </AwesomeButton>
+      }/>
+    </div>
+    </>
+  );
+};
+
+
+
+
+const handleExternalLinks = () => {
+  const allLinks = Array.from(document.querySelectorAll('a'));
+  if (allLinks.length > 0) {
+    allLinks.forEach(link => {
+      if (link.host !== window.location.host) {
+        link.setAttribute('rel', 'noopener noreferrer');
+        link.setAttribute('target', '_blank');
+      }
+    });
+  }
+};
+
+const { type, num } = getRandomType();
+
 
 const Home = () => {
-
-
-  const buttonData = [
-    {
-      text: 'What?',
-      row: 1,
-      toast: Explanation,
-      className: 'gold'
   
-    },
-    {
-      text: 'Portfolio',
-      link: 'https://btschwartz.com/portfolio/',
-      row: 2,
-      className: 'purple'
-  
-    },
-    {
-      text: 'Resume',
-      link: 'https://drive.google.com/file/d/1wCPzd7fiAko-PfaizeCkd8ZChVdLK7eA/view?usp=sharing',
-      row: 2,
-      className: 'black'
-    },
-    
-  
-    {
-      text: 'More',
-      row: 3,
-      className: 'gray',
-      navLink: '/more',
-      link: '/more'
-
-    },
-    
-    {
-      text: 'Contact',
-      row: 3,
-      className: 'red',
-      modal: ContactModal,
-      modalId: 'contact',
-      modalProps: {
-      },
-    }
-  ]
-
-  const { type, num } = getRandomType();
 
   const [isLoading, setIsLoading] = useState(true);
+  const [cancel, setCancel] = useState(false);
 
   const { hasAnimated, setHasAnimated } = useContext(AnimationContext);
 
   const location = useLocation();
 
-
-  const handleExternalLinks = () => {
-    const allLinks = Array.from(document.querySelectorAll('a'));
-    if (allLinks.length > 0) {
-      allLinks.forEach(link => {
-        if (link.host !== window.location.host) {
-          link.setAttribute('rel', 'noopener noreferrer');
-          link.setAttribute('target', '_blank');
-        }
-      });
-    }
-  };
+  const navigate = useNavigate();
 
 
   useEffect(() => {
@@ -160,30 +218,49 @@ const Home = () => {
     handleExternalLinks();
   }, [isLoading, location]);
 
+
+
+  const redirectToPortfolio = () => {
+    // window.location.href = "https://btschwartz.com/portfolio/";
+    navigate('/portfolio')
+  }
+
+  const cancelCountdown = () => {
+    setCancel(true);
+  };
+
+  const finishLoading = () => {
+    setIsLoading(false);
+    setHasAnimated(true);
+  };
+
   return (
     <>
-    {isLoading && !hasAnimated ? (
-      <Loader finishLoading={() => { setIsLoading(false); setHasAnimated(true); }} />
-    ) : (
-    
-      <div className="daylight" style={styles.bg}>
-
-        <div className="default" >
-          <main className="App-main">
-            
-            <Title />
-            <Tagline />
-            <Buttons buttonData={buttonData} effect="zoomIn"/>
-            <Toaster />
-            <ParticlesBg type={type} bg={true} num={num} 
-            styles={{backgroundColor: 'black'}}
-            />
-          </main>
-
-        </div>
-        
-      </div>
-    )}
+      {isLoading && !hasAnimated ? (
+        <Loader finishLoading={finishLoading} />
+      ) : (
+        <>
+        <ParticlesBg type={type} bg={true} num={num} styles={{ backgroundColor: 'black' }} />
+          <div className="daylight" style={styles.bg}>
+            <div className="default">
+              <main className="App-main">
+                {cancel ? (
+                <>
+                <Title />
+                <Tagline />
+                <Buttons buttonData={buttonData} effect="zoomIn" />
+                <Toaster />
+                </>
+                ) : (
+                  <>
+                    <Countdown onEnd={redirectToPortfolio} onCancel={cancelCountdown} />
+                  </>
+                )}
+              </main>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
